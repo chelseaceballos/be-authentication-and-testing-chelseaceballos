2bbb41ad18158403d116369dc8270c1502bbee1d
Chelsea Ceballos <chelsea-ceballos@lambdastudents.com>
2021-08-19 21:42:49 -0700

    [GET] /api/JOKES 2 test passing

--- api/server.test.js
+++ api/server.test.js
@@ -30,7 +30,7 @@ describe('IF THESE FAIL, ITS OVER', () =>{
 describe('[POST] /api/auth/REGISTER', () => {
   it('cannot register with missing credentials', async () => {
     let res = await request(server).post('/api/auth/register').send({username: 'bar'}) // missing password
-    expect(res.status).toBe(400)
+    expect(res.status).toBe(400) // bad request
   })
   it('res status 201 if credentials are both valid', async () => {
     let res = await request(server).post('/api/auth/register').send({username: 'bar', password: '1234'})
@@ -38,14 +38,14 @@ describe('[POST] /api/auth/REGISTER', () => {
   })
 })
 
-describe('[POST] /api/auth/LOGIN', async () => {
+describe('[POST] /api/auth/LOGIN', () => {
   it('resolves in sad path if incorrect password is entered status: 401', async () => {
    //register first
     let register = await request(server).post('/api/auth/register').send({username: 'bar', password: '1234'})
    // login with  wrong pw
     let login = await request(server).post('/api/auth/login').send({username: 'bar', password: 'bar'})
   // assert
-    expect(login.status).toBe(401)
+    expect(login.status).toBe(401) // unauthorized
     expect(login.body.message).toMatch("invalid credentials")
   })
   it('can login status:200', async () => {
@@ -57,6 +57,18 @@ describe('[POST] /api/auth/LOGIN', async () => {
 })
 
 describe('[GET] /api/JOKES ', () => {
-
+  it('restricts jokes if not logged in :( ', async () => {
+    //user tries to immediately get jokes without register/login
+    const jokes = await request(server).get('/api/jokes')
+    expect(jokes.status).toBe(401) //unauthorized
+    expect(jokes.body.message).toMatch("Token required")
+  })
+  it('can see jokes if auth', async () => {
+    let register = await request(server).post('/api/auth/register').send({username: 'bar', password: '1234'})
+    let login = await request(server).post('/api/auth/login').send({username: 'bar', password: '1234'})
+    let jokes = await request(server).get('/api/jokes').set('authorization', login.body.token) 
+    expect(jokes.status).toBe(200)
+    expect(jokes.body).toHaveLength(3)
+  })
 })
 
